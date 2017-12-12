7a7d9120341273b2fc40f8f868bc785b45ccd2a8
SergeyZaykov <34367686+SergeyZaykov@users.noreply.github.com>
2017-12-12 05:47:52 +0300

    первая версия

---
+++ SQL2DAL.py
@@ -0,0 +1,19 @@
+﻿# -*- coding: utf-8 -*-
+# vi:ts=4:et
+
+SQL='''SELECT "Секретарь", "номер группы", "Нет оценки рецензента", "Нет рецензента", 
+       "Оценка без рецензента", "Без дня защиты"
+  FROM "Диплом"."Статистика для секретаря";'''
+import parse
+filtred_SQL=SQL.replace("\n","") # удалить символы новой строки
+поля,таблица=parse.parse("SELECT {} FROM {};",filtred_SQL) # выделить список полей и имя таблицы
+fields=поля.split(",")
+# объявление таблицы для DAL
+declare4DAL='''db.define_table('',
+%s
+                rname='%s',
+                primarykey=[''],
+                migrate=False
+               )'''%("\n".join(['''Field('',rname='%s',type='string',label=%s),'''%(field.strip(),field.strip(),) for field in поля.split(",")]),таблица.strip(),)
+
+print(declare4DAL)
